9d3a3ba98eba0f40bfe08ace4f7e68d0339d4a4a
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-03-13 17:34:27 +0900

    feat: picoctf_2024/reverse_engineering/packer

---
+++ picoctf_2024/reverse_engineering/packer/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  long long local_88 = 0x6636333639363037;
+  long long local_80 = 0x6237363434353334;
+  long long local_78 = 0x6635383539333535;
+  long long local_70 = 0x3433303565363535;
+  long long local_68 = 0x6534313362363336;
+  long long local_60 = 0x3133323466353633;
+  long long local_58 = 0x3936323534336536;
+  long long local_50 = 0x3533663533353333;
+  long long local_48 = 0x3433353635363436;
+  long long local_40 = 0x6437313334333433;
+  long long local_38 = 0;
+
+  printf("%s\n", (char*)&local_88);
+}
+// "Password correct, please see flag: 7069636f4354467b5539585f556e5034636b314e365f42316e34526933535f35646565343434317d"
